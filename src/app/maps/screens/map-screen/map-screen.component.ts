879ed9ebae6b15be5aa5e900cb62696bfe255c32
Duvan <duvanriano2626@gmail.com>
2024-09-11 16:19:38 -0500

    Creacion de los componentes de busqueda y detalles de la busqueda

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -4,11 +4,12 @@ import { LoadingComponent } from '../../components/loading/loading.component';
 import { MapsViewComponent } from "../../components/maps-view/maps-view.component";
 import { AngularLogoComponent } from "../../components/angular-logo/angular-logo.component";
 import { BtnMyLocationComponent } from '../../components/btn-my-location/btn-my-location.component';
+import { SearchBarComponent } from '../../components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-map-screen',
   standalone: true,
-  imports: [LoadingComponent, MapsViewComponent, AngularLogoComponent,BtnMyLocationComponent],
+  imports: [LoadingComponent, MapsViewComponent, AngularLogoComponent,BtnMyLocationComponent, SearchBarComponent],
   templateUrl: './map-screen.component.html',
   styleUrl: './map-screen.component.css'
 })
